124b659281661f7b998089300d69a0953bb3f050
Marco Cafasso <marcocafassog@gmail.com>
2022-12-20 14:33:20 +0100

    test: adds photos in the application
    - creates six instances of Photo
    - prints in console with findAll method to verify entries

--- Photobook/src/main/java/org/generation/italy/demo/PhotobookApplication.java
+++ Photobook/src/main/java/org/generation/italy/demo/PhotobookApplication.java
@@ -1,13 +1,43 @@
 package org.generation.italy.demo;
 
+import org.generation.italy.demo.pojo.Photo;
+import org.generation.italy.demo.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhotobookApplication {
-
+public class PhotobookApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PhotoService photoService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotobookApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// ADDING PHOTOS
+		
+		Photo p1 = new Photo("Jimi Hendrix", "Guitar art music Painting Wall", "https://i.pinimg.com/564x/77/01/8c/77018cb58dbe090629b29ec522e88033.jpg", "painting", true);
+		Photo p2 = new Photo("Cowboy Bebop", "Took the popular Spike \"Bang\" phone wallpaper and touched it up a bit", "https://i.pinimg.com/564x/19/0d/62/190d62d4ddb3b0dc36ce5a78e0d9da09.jpg", "wallpaper", true);
+		Photo p3 = new Photo("Comic Art", "Cool comic art on twitter", "https://i.pinimg.com/564x/88/e6/ef/88e6ef7ec6ae3ba421e87cf4bece891a.jpg", "comic", true);
+		Photo p4 = new Photo("Comic Cyberpunk", "Cyberpunk comic style on twitter", "https://i.pinimg.com/564x/df/95/4f/df954fb7957a9e3b23f0efa22f5e0dab.jpg", "comic", true);
+		Photo p5 = new Photo("Wallpaper Walking", "Walk on skyline artistic wallpaper", "https://i.pinimg.com/564x/bd/8a/c3/bd8ac396f7e6d703bc3b9addc690b6f8.jpg", "wallpaper", true);
+		Photo p6 = new Photo("Land of Sin", "Science fiction art in apocalypse future", "https://i.pinimg.com/564x/05/f1/00/05f100fa3026d62ddfd312b038415ee8.jpg", "scifiart", true);
+		
+		photoService.save(p1);
+		photoService.save(p2);
+		photoService.save(p3);
+		photoService.save(p4);
+		photoService.save(p5);
+		photoService.save(p6);
+		
+		System.err.println(photoService.findAll());
+	}
+	
+	
 }
