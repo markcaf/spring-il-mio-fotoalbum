d32c93b750a218c21d7129d2fecdd6fca9363888
Marco Cafasso <marcocafassog@gmail.com>
2022-12-20 10:40:01 +0100

    init: setup spring project

---
+++ Photobook/src/main/java/org/generation/italy/demo/PhotobookApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotobookApplication.class, args);
+	}
+
+}
---
+++ Photobook/src/test/java/org/generation/italy/demo/PhotobookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
