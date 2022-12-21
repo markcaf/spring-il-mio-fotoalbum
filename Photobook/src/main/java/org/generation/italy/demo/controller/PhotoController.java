6559f862a814a7453d30517ba0828f2ac3f1beed
Marco Cafasso <marcocafassog@gmail.com>
2022-12-21 01:07:41 +0100

    feat: adds show method + view
    - adds in PhotoController the show method which returns the view of the single photo
    - prints photo info based on ID with related categories

--- Photobook/src/main/java/org/generation/italy/demo/controller/PhotoController.java
+++ Photobook/src/main/java/org/generation/italy/demo/controller/PhotoController.java
@@ -1,13 +1,16 @@
 package org.generation.italy.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.generation.italy.demo.pojo.Category;
 import org.generation.italy.demo.pojo.Photo;
 import org.generation.italy.demo.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +25,21 @@ public class PhotoController {
 		List<Photo> photos = photoService.findAllWCategory();
 		model.addAttribute("photos", photos);
 		return "photo-index";
+	}
+	
+	@GetMapping("/photo/{id}")
+	public String show(@PathVariable("id") int id, Model model) {
+		Optional<Photo> optionalPhoto = photoService.findPhotoById(id);
+		if (optionalPhoto.isEmpty()) {
+			return "404";
+		}
+
+		Photo photo = optionalPhoto.get();
+		List<Category> categories = photo.getCategories();
+
+		model.addAttribute("photo", photo);
+		model.addAttribute("categories", categories);
+
+		return "photo-view";
 	}
 }
