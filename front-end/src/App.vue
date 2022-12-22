79ab08886847b26c9c7840be069c6ed4e7ef3ae0
Marco Cafasso <marcocafassog@gmail.com>
2022-12-22 12:58:35 +0100

    feat: adds HeaderComponent in partials folder
    - import of HeaderComponent in App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
+    <HeaderComponent />
     <h1 class="text-center">PHOTOBOOK</h1>
   </div>
 </template>
 
 <script>
-
+import HeaderComponent from './components/partials/HeaderComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    HeaderComponent,
   }
 }
 </script>
---
+++ front-end/src/components/partials/HeaderComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active btn btn-dark px-3 rounded-pill text-white me-2" aria-current="page" href="#">Home Page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Photos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Categories</a>
+                    </li>
+                </ul>
+                
+                <form class="container-fluid w-75" id="search_bar">
+                    <div class="input-group">
+                    <span class="input-group-text" id="basic-addon1">Search</span>
+                    <input type="text" class="form-control" placeholder="Insert a title or a tag to search between photos" aria-label="Search" aria-describedby="basic-addon1">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    img {
+        max-width: 40px;
+    }
+
+</style>
